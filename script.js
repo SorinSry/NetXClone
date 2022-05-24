c2aa1f5c2a605f10124a47b82b0d3be766c20019
SorinSry <sorin.agrotrip@gmail.com>
2022-05-24 16:04:20 +0300

    Initial Commit

---
+++ script.js
@@ -0,0 +1,35 @@
+const buttonItems = document.querySelectorAll('.btn-faq');
+const buttonContentItems = document.querySelectorAll('.btn-content');
+
+// Listen for button click
+buttonItems.forEach(item => {
+  item.addEventListener('click', selectButton);
+});
+
+// Select button item
+function selectButton(e) {
+  removeIconRotate(this.id);
+  this.lastElementChild.classList.toggle('icon-rotate');
+
+  removeShow(`${this.id}-content`);
+  const buttonContent = document.querySelector(`#${this.id}-content`);
+  buttonContent.classList.toggle('showfaq');
+}
+
+// Remove icon-rotate class from all icon items
+function removeIconRotate(selectionId) {
+  buttonItems.forEach(item => {
+    if (item.id != selectionId) {
+      item.lastElementChild.classList.remove('icon-rotate');
+    }
+  });
+}
+
+// Remove show class from al button items
+function removeShow(selectionId) {
+  buttonContentItems.forEach(item => {
+    if (item.id != selectionId) {
+      item.classList.remove('show');
+    }
+  });
+}
